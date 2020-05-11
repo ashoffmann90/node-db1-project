ccbcb5a8263e4b2c150f8bc11a230c75a1c021e8
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-05-11 14:20:16 -0700

    stretch basic queries

--- queries.sql
+++ queries.sql
@@ -24,6 +24,7 @@ where city = "London" or city = "Madrid" or country = "Brazil"
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 insert into customers (customerName, contactName, address, postalCode, city, country)
 values ("The Shire", "Bilbo Baggins", "1 Hobbit-Hole", "111", "Bag End", "Middle Earth")
+;
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
 update customers
@@ -32,7 +33,12 @@ where ContactName = "Bilbo Baggins"
 ;
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
-
+select * from customers
+group by city
+;
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+select * from suppliers
+where length(supplierName) > 20
+;
 
